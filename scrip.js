608415f8c1a967c81e0c5c1b656d66d40fb32c7f
tuhinalhelal10 <mdtuhinhelal10@gamil.com>
2022-09-21 14:50:31 +0600

    This  is my first personal website

---
+++ scrip.js
@@ -0,0 +1,16 @@
+// typing animation
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Video Editor"],
+    typeSpeed:300,
+    BackSpeed:200,
+    loop:true
+})
+
+
+var typed = new Typed(".typing2",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Video Editor"],
+    typeSpeed:300,
+    BackSpeed:200,
+    loop:true
+})
